b5bd617139687f3d2b7f373b2e11858b099298dd
krito20 <capaciteseparaelexito@gmail.com>
2023-09-09 09:16:38 -0500

    Visualizaciones Codificadas

--- app.py
+++ app.py
@@ -6,12 +6,48 @@ from flask import Flask, request, jsonify, render_template, url_for
 import pickle
 from sklearn import svm
 import sys
-import seaborn as sns
-import matplotlib.pyplot as plt
-import matplotlib as mpl
+import logging
+import os
+from typing import Union
+from google.cloud import storage
+from io import StringIO
+import altair as alt
+import openpyxl
+
 
 #-------------------------------------------------------IAP GCP
 app = Flask(__name__)
+
+#----------------------------------------------------B
+# Configure this environment variable via app.yaml
+#CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
+
+@app.route('/')
+def index() -> str:
+    return """
+<form method="POST" action="/upload" enctype="multipart/form-data">
+    <input type="datos" name="datos">
+    <input type="submit">
+</form>
+"""
+@app.route('/upload', methods=['POST'])
+def upload(csvdata, bucketname, blobname):
+    client = storage.Client()
+    bucket = client.get_bucket(bucketname)
+    blob = bucket.blob(blobname)
+    blob.upload_from_string(csvdata)
+    gcslocation = 'gs://{}/{}'.format(bucketname, blobname)
+    logging.info('Uploaded {} ...'.format(gcslocation))
+    return gcslocation
+
+@app.errorhandler(500)
+def server_error(e: Union[Exception, int]) -> str:
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+#-----------------------------------------------------
 
 CERTS = None
 AUDIENCE = None
@@ -111,13 +147,13 @@ def main():
             model = pickle.load(file)
     
     # Configura titulo e icon de pagina        
-    st.set_page_config(page_title="Modelo Analítica ELX", page_icon="img/Icono.ico", layout="wide")
+    st.set_page_config(page_title="Modelo Analítico ELX", page_icon="img/Icono.ico", layout="wide")
 
     # Menú y logo
     st.sidebar.image("img/logo3.png")
     st.sidebar.write("")
 
-
+    #Estilo botón
     st.markdown("""
             <style>
             div.stButton > button:first-child {
@@ -130,49 +166,51 @@ def main():
             }
             </style>""", unsafe_allow_html=True)
 
-
     a, b, c = False, False, False
-    datos =''
-    # Title
-    #st.title("Customer Analytics")
                 
-
-    with st.sidebar.expander("CUSTOMER JOURNEY: ", expanded = False):
-        #if st.sidebar.button("Mi primer opción Modelo"):
-        a = st.button("Visualizar",type="primary")
-
-
-    with st.sidebar.expander("MODELO MÚLTIPLES CLIENTES: ", expanded = False):
-        #if st.sidebar.button("Mi primer opción Modelo"):
+    with st.sidebar.expander("MODELO MÚLTIPLES CLIENTES ", expanded = False):
         # uploading files
-        datos = st.file_uploader("Subir archivos: ", type = ["csv"])
-        print(type(datos))
-        #st.image(data)
+        datos = st.file_uploader("Subir archivos: ", type = ["xlsx"])
+ 
+        if datos is not None:
+            # To read file as bytes:
+            ##bytes_data = datos.getvalue()
+            
+            # To convert to a string based IO:
+            ##stringio = StringIO(datos.getvalue().decode("utf-8"))
+
+            # To read file as string:
+            ##string_data = stringio.read()
+
+            # Can be used wherever a "file-like" object is accepted:
+            dataframe = pd.read_excel(datos,index_col=False)
+            
+            #subir archivo al bucket en gcloud
+            #urlarchivo = upload(bytes_data,CLOUD_STORAGE_BUCKET,'datos')
+        
         b = st.button("Ejecutar Modelo",type="primary")
 
 
-    with st.sidebar.expander("MODELO RECOMENDACIÓN UNITARIA: ", expanded = False):
-        #if st.sidebar.button("Mi primer opción Modelo"):
-        # select boxes
-        #selec_item = st.selectbox("Variable 1", ["A","B","C","D"])
-        #selec_item = st.selectbox("Variable 2", ["1","3","6","9"])
-        #selec_item = st.selectbox("Variable 3", ["Ltda","S.A.S"])
-        # slider
-        #range_var = st.slider("Variable 5:", 1,5)
-        #range_var = st.slider("Variable 6:", 1000000,5000000)
-        
-        # Lecctura de datos
-
-        N = st.text_input("Nitrógeno:",placeholder=94)
-        P = st.text_input("Fósforo:",placeholder=53)
-        K = st.text_input("Potasio:",placeholder=40)
-        Temp = st.text_input("Temperatura:",placeholder=20.2)
-        Hum = st.text_input("Humedad:",placeholder=82.9)
-        pH = st.text_input("pH:",placeholder=5.7)
-        rain = st.text_input("Lluvia:",placeholder=241.9)
+    with st.sidebar.expander("MODELO UNITARIO ", expanded = False):
+        # Lectura de datos
+        nit = st.number_input("Digite el número del Nit:",value=1,max_value=999999999)
+        actEcon = st.selectbox("Actividad económica:",['Industrial','Servicios','Transporte',
+                               'Comercio','Construcción','Energético','Financiero','Comunicaciones','Agropecuario'])
+        tamEmp = st.selectbox("Tamaño de la empresa:",['Gran Empresa','Mediana Empresa','Pequeña Empresa'])
+        flegal = st.selectbox("Forma Legal:",['S.A.S.','LTDA.','S.A.','ESAL','Persona Jurídica','Sucursal Extranjera',
+                                              'S.C.A.','Undefined','Persona Natural','S.C.S.'])
+        numEmpl = st.number_input("Número de empleados:",value=1)
+        activos = st.number_input("Activos Totales:")
+        ingresosOp = st.number_input("Total Ingresos Operativos:")
+        TotPatr = st.number_input("Total Patrimonio:")
+        ganDespImpto = st.number_input("Ganancias después de Impuestos:")
+        edadEmp = st.number_input("Edad de la Empresa:",value=1)
                 
         # button
-        c = st.button("Ejecutar Modelo2",type="primary")
+        c = st.button("Ejecutar Modelo 1",type="primary")
+        
+    with st.sidebar.expander("CUSTOMER JOURNEY ", expanded = False):
+        a = st.button("Visualizar",type="primary")        
 
 
     if a == False and b == False and c == False:
@@ -202,154 +240,18 @@ def main():
             ## Volume Price
             """)
             col2.line_chart(tickerDf.Volume)
-            
-            #-----------------------------------------------------------------
-            
-            class Visuals():
-  
-                def __init__(self,df):
-                    self.df = df
-
-                def ValidarCampos(self):
-                    lista_campos = ['RangoConsumo','RangodeCompra($)','RangoRecurrenciaCompra','ClusterComprados',
-                                'TipoCliente#Oportunidades','TipoCliente$Oportunidades','CategorizacionSectores',
-                                'ESTATUSOPERACIONAL','TamanoEmpresa','CategoriaDepartamento']
-                    for campo in lista_campos:
-                        if campo in df.columns.to_list():
-                            pass
-                        else: 
-                                print('El campo ', '"'+campo+'"', ' no se encuentra en el archivo cargado, por favor valide que el archivo contenga el campo ', '"'+campo+'"')
-                
-                # def Validar_categorias_por_campo(self):
-                #   cat_rango
-
-                def Graficar(self, variable ):
-                    # variable 
-                    mpl.rcParams['font.size'] = 12
-                    mpl.rcParams['font.family'] = 'sans-serif'
-                    color = '#461e7d'
-                    df_f = df.copy()
-                    df_f[variable].fillna('Sin Catalogar', inplace=True)                 
-
-                    orden = ['Sin Catalogar', 'Menor a 5000', 'Entre 5000 y 10000', 'Entre 10000 y 55000', 'Mayor a 55000']
-
-                    values = df_f[variable].value_counts().keys().to_list()
-                    n_barras = len(values)
-                    
-                    #total = df_f.count(axis=1)
-                    #fig = plt.barh(total.index, total)
-                    #st.pyplot(fig)
-
-                    plt.figure(figsize = (7,5))
-
-                    ax = sns.countplot(y=variable, order = orden, data=df_f, color = color #[(70/255,30/255,125/255)]
-                                    , orient = 'h')#, hue = 'Tipo de usuario', hue_order=df["Tipo de usuario"].value_counts().keys().tolist())
-
-                    for i in range(n_barras):
-                        p = ax.patches[i]
-                        total = len(df_f) 
-                    
-                    ax.annotate(format(p.get_width(), '.0f') + ' ('+format(p.get_width()/total*100, '.2f')+' %)', 
-                                (p.get_width(), p.get_y() + p.get_height()), # (p.get_x() + p.get_width() / 2., p.get_height())
-                                ha='center', va='center',
-                                xytext=(50, 20),
-                                textcoords='offset points')
-
-
-                    ax.spines['top'].set_visible(False)
-                    ax.spines['right'].set_visible(False)
-                    ax.spines['bottom'].set_visible(False)
-                    ax.spines['left'].set_visible(False)
-
-                    plt.xticks([])
-                    # plt.axis('off')
-                    plt.title('')
-                    plt.xlabel('Cantidad de Registros')
-                    
-                    if variable == 'RangoConsumo':
-                        plt.ylabel('Rango de Consumo')
-
-                        #plt.show()
-                    
-            sheet_name = 'Data'
-            path = './Ejemplo_prueba_cartera.xlsx'
-            df = pd.read_excel(path, sheet_name= sheet_name)
-
-            ob = Visuals(df)
-            ob.ValidarCampos()
-            ob.Graficar('RangoConsumo')
 
     elif b == True:
-        st.title("Modelo Múltiples Registros")
-        tab1, tab2, tab3,tab4 = st.tabs(["CONSUMO", "VENTAS", "ECONOMICAS","DEMOGRÁFICAS"])
-        with tab1:
-            st.write("")
-            st.subheader("CONSUMO")
-            col7,col8 = st.columns(2) 
-            st.image("img/cons.png",use_column_width="always")
-            st.write("")
-        with tab2:
-            st.write("")
-            st.subheader("VENTAS")
-            col1,col2 = st.columns(2)     
-            col1.image("img/ven1.png")
-            col2.image("img/ven2.png")
-            col1.image("img/ven5.png")
-            col2.image("img/ven6.png")
-            col1.image("img/ven3.png")
-            #col3.image("img/Ventas6.png")
-            st.write("")
-        with tab3:
-            st.write("")
-            st.subheader("ECONOMICAS")
-            col4,col5 = st.columns(2)     
-            col4.image("img/econo1.png")
-            col5.image("img/econo3.png")
-            col4.image("img/econo2.png")
-            st.write("")
-        with tab4:
-            st.write("")
-            st.subheader("DEMOGRÁFICAS")
-            col9,col0 = st.columns(2)
-            st.image("img/dem.png",use_column_width="always")
-            st.write("")
-            
-        #st.image("img/Modelo2.png",use_column_width="always")
-        #st.write("")
-        
-        dataframe = pd.read_csv(datos)
-        #st.write(dataframe.head())
-        
-        def convert_df(df):
-            # IMPORTANT: Cache the conversion to prevent computation on every rerun
-            return df.to_csv().encode('utf-8')
-
-        csv = convert_df(dataframe)
-
-        st.download_button(
-            label="Descargar archivo",
-            data=csv,
-            file_name='large_df.csv',
-            mime='text/csv',
-        )
-        #st.download_button("Descargar archivo",data=datos)
-        
-    elif c == True:
-        #st.image("img/Modelo1.png",use_column_width="always")
-        #st.write("")
-        x_in =[np.float_(N.title()),
-                    np.float_(P.title()),
-                    np.float_(K.title()),
-                    np.float_(Temp.title()),
-                    np.float_(Hum.title()),
-                    np.float_(pH.title()),
-                    np.float_(rain.title())]
-        predictS = model_prediction(x_in, model)
-        st.success('EL CULTIVO RECOMENDADO ES: {}'.format(predictS[0]).upper())
+        st.markdown("<h1 style='text-align: center;'>Modelo Analítico ELX</h1>", unsafe_allow_html=True)
         st.write("")
-        if datos != '':
-            dataframe = pd.read_csv(datos)
-            st.write(dataframe.head())
+        vista1,vista2 = st.tabs(["RESULTADO MODELO ELX", "REPORTE DESCRIPTIVO"])
+        
+        with vista1:
+            #dataframe = pd.read_csv(datos)
+            
+            #dataframe = pd.read_csv('./Crop_recommendation.csv','r')
+            st.write("")
+            st.write(dataframe)
             
             def convert_df(df):
                 # IMPORTANT: Cache the conversion to prevent computation on every rerun
@@ -360,9 +262,98 @@ def main():
             st.download_button(
                 label="Descargar archivo",
                 data=csv,
-                file_name='large_df.csv',
+                file_name='Resultado_df.csv',
                 mime='text/csv',
             )
+            
+        with vista2:
+            tab1, tab2, tab3,tab4 = st.tabs(["Consumo", "Ventas", "Económico","Demográficas"])
+            source = dataframe
+            with tab1:
+                st.write("")
+                st.write("")
+                st.subheader("CONSUMO")
+                col7,col8 = st.columns(2) 
+                #col8.image("img/cons.png",use_column_width="always")
+                bar= alt.Chart(source).mark_bar().encode(x='count()',y="RangoConsumo:N").configure_mark(color='#566573')
+                col7.altair_chart(bar,use_container_width=True,theme="streamlit")
+                st.write("")
+            with tab2:
+                st.write("")
+                st.write("")
+                st.subheader("VENTAS")
+                col1,col2 = st.columns(2, gap="medium")     
+                #col1.image("img/ven1.png")
+                bar= alt.Chart(source).mark_bar().encode(x='count()',y="RangodeCompra($):N").configure_mark(color='#566573')
+                col1.altair_chart(bar,use_container_width=True,theme="streamlit")
+                #col2.image("img/ven2.png")
+                bar= alt.Chart(source).mark_bar().encode(x='count()',y="RangoRecurrenciaCompra:N").configure_mark(color='#566573')
+                col2.altair_chart(bar,use_container_width=True,theme="streamlit")
+                #col1.image("img/ven5.png")
+                bar= alt.Chart(source).mark_bar().encode(x='count()',y="TipoCliente#Oportunidades:N").configure_mark(color='#566573')
+                col1.altair_chart(bar,use_container_width=True,theme="streamlit")
+                #col2.image("img/ven6.png")
+                bar= alt.Chart(source).mark_bar().encode(x='count()',y="TipoCliente$Oportunidades:N").configure_mark(color='#566573')
+                col2.altair_chart(bar,use_container_width=True,theme="streamlit")
+                #col1.image("img/ven3.png")
+                bar= alt.Chart(source).mark_bar().encode(x='count()',y="ClusterComprados:N").configure_mark(color='#566573')
+                col1.altair_chart(bar,use_container_width=True,theme="streamlit")
+                st.write("")
+            with tab3:
+                st.write("")
+                st.write("")
+                st.subheader("ECONOMICAS")
+                col4,col5 = st.columns(2,gap="medium")  
+                #col5.image("img/econ3.png")
+                bar= alt.Chart(source).mark_bar().encode(x='count()',y="TamanoEmpresa:N").configure_mark(color='#566573')
+                col4.altair_chart(bar,use_container_width=True,theme="streamlit")   
+                #col4.image("img/econ1.png")
+                bar= alt.Chart(source).mark_bar().encode(x='count()',y="CategorizacionSectores:N").configure_mark(color='#566573')
+                col5.altair_chart(bar,use_container_width=True,theme="streamlit")
+                #col4.image("img/econ2.png")
+                bar= alt.Chart(source).mark_bar().encode(x='count()',y="EstatusOperacional:N").configure_mark(color='#566573')
+                col4.altair_chart(bar,use_container_width=True,theme="streamlit")
+                st.write("")
+            with tab4:
+                st.write("")
+                st.write("")
+                st.subheader("DEMOGRÁFICAS")
+                col9,col0 = st.columns(2)
+                #col9.image("img/dem.png",use_column_width="always")
+                bar= alt.Chart(source).mark_bar().encode(x='count()',y="CategoriaDepartamento:N").configure_mark(color='#566573')
+                col9.altair_chart(bar,use_container_width=True,theme="streamlit")
+                st.write("")
+                
+
+    elif c == True:
+        st.image("img/Modelo1.png",use_column_width="always")
+        #st.write("")
+        #x_in =[np.float_(N.title()),
+        #            np.float_(P.title()),
+        #            np.float_(K.title()),
+        #            np.float_(Temp.title()),
+        #            np.float_(Hum.title()),
+        #            np.float_(pH.title()),
+        #            np.float_(rain.title())]
+        #predictS = model_prediction(x_in, model)
+        #st.success('EL CULTIVO RECOMENDADO ES: {}'.format(predictS[0]).upper())
+        #st.write("")
+        #if datos != '':
+        #    dataframe = pd.read_csv(datos)
+        #    st.write(dataframe.head())
+            
+        #    def convert_df(df):
+                # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        #        return df.to_csv().encode('utf-8')
+
+        #    csv = convert_df(dataframe)
+            
+        #    st.download_button(
+        #        label="Descargar archivo",
+        #        data=csv,
+        #        file_name='large_df.csv',
+        #        mime='text/csv',
+        #    )
         #st.download_button("Descargar archivo",data=datos)
 
 if __name__ == '__main__':
